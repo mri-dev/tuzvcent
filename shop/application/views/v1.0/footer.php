799abbad6e5214d1b99001b09ec38325df2ef976
mri-dev <pocokxtrame@gmail.com>
2017-12-15 21:18:30 +0100

    Save

--- shop/application/views/v1.0/footer.php
+++ shop/application/views/v1.0/footer.php
@@ -29,8 +29,8 @@
 			<div class="pw">
 				<div class="flex">
 					<div class="logo">
-						logo
-					</div>
+	          <a href="<?=$this->settings['page_url']?>"><img src="<?=IMG?>tuzvedelmicentrum_logo_white.svg" alt="<?=$this->settings['page_title']?>"></a>
+	        </div>
 					<div class="navi">
 						nav
 					</div>
@@ -46,11 +46,66 @@
 					<div class="copy flex-col-40">
 						&copy; <span class="author"><?=$this->settings['page_title']?></span>: <?=$this->settings['page_description']?> &nbsp;&nbsp; 2013 Minden jog fenntartva!
 					</div>
-					<div class="contact flex-col-40">
-						contacts
+					<div class="contact">
+						<div class="flex">
+							<div class="telefon">
+								<div class="wrapper">
+									<i class="fa fa-phone"></i>
+									<div class="title">
+										Telefon:
+									</div>
+									<div class="val">
+										<a href="tel:<?=$this->settings['page_author_phone']?>"><?=$this->settings['page_author_phone']?></a>
+									</div>
+								</div>
+							</div>
+							<div class="email">
+								<div class="wrapper">
+									<i class="fa fa-envelope-o"></i>
+									<div class="title">
+										E-mail:
+									</div>
+									<div class="val">
+										<a href="mailto:<?=$this->settings['primary_email']?>"><?=$this->settings['primary_email']?></a>
+									</div>
+								</div>
+							</div>
+							<div class="address">
+								<div class="wrapper">
+									<i class="fa fa-map-marker"></i>
+									<div class="title">
+										Cím:
+									</div>
+									<div class="val">
+										<?=$this->settings['page_author_address']?>
+									</div>
+								</div>
+							</div>
+						</div>
 					</div>
-					<div class="social flex-col-20">
-						social
+					<div class="social flex-col-15">
+						<div class="flex flexmob-exc-resp">
+							<?php if ( !empty($this->settings['social_facebook_link'])) : ?>
+							<div class="facebook">
+								<a target="_blank" title="Facebook oldalunk" href="<?=$this->settings['social_facebook_link']?>"><i class="fa fa-facebook"></i></a>
+							</div>
+							<?php endif; ?>
+							<?php if ( !empty($this->settings['social_youtube_link'])) : ?>
+							<div class="youtube">
+								<a target="_blank" title="Youtube csatornánk" href="<?=$this->settings['social_youtube_link']?>"><i class="fa fa-youtube"></i></a>
+							</div>
+							<?php endif; ?>
+							<?php if ( !empty($this->settings['social_googleplus_link'])) : ?>
+							<div class="googleplus">
+								<a target="_blank" title="Google+ oldalunk" href="<?=$this->settings['social_googleplus_link']?>"><i class="fa fa-google-plus"></i></a>
+							</div>
+							<?php endif; ?>
+							<?php if ( !empty($this->settings['social_twitter_link'])) : ?>
+							<div class="twitter">
+								<a target="_blank" title="Twitter oldalunk" href="<?=$this->settings['social_twitter_link']?>"><i class="fa fa-twitter"></i></a>
+							</div>
+							<?php endif; ?>
+						</div>
 					</div>
 				</div>
 			</div>
--- shop/application/views/v1.0/header.php
+++ shop/application/views/v1.0/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <html xmlns="http://www.w3.org/1999/html4"
       xmlns:og="http://ogp.me/ns#"
-      xmlns:fb="http://www.facebook.com/2008/fbml" lang="hu-HU" ng-app="casada">
+      xmlns:fb="http://www.facebook.com/2008/fbml" lang="hu-HU" ng-app="tuzvedelmicentrum">
 <head>
     <title><?=$this->title?></title>
     <?=$this->addMeta('robots','index,folow')?>
@@ -32,12 +32,173 @@
 <header>
   <div class="top">
     <div class="pw">
-      ---
+      <div class="flex">
+        <div class="social">
+          <div class="flex flexmob-exc-resp">
+            <?php if ( !empty($this->settings['social_facebook_link'])) : ?>
+            <div class="facebook">
+              <a target="_blank" title="Facebook oldalunk" href="<?=$this->settings['social_facebook_link']?>"><i class="fa fa-facebook"></i></a>
+            </div>
+            <?php endif; ?>
+            <?php if ( !empty($this->settings['social_youtube_link'])) : ?>
+            <div class="youtube">
+              <a target="_blank" title="Youtube csatornánk" href="<?=$this->settings['social_youtube_link']?>"><i class="fa fa-youtube"></i></a>
+            </div>
+            <?php endif; ?>
+            <?php if ( !empty($this->settings['social_googleplus_link'])) : ?>
+            <div class="googleplus">
+              <a target="_blank" title="Google+ oldalunk" href="<?=$this->settings['social_googleplus_link']?>"><i class="fa fa-google-plus"></i></a>
+            </div>
+            <?php endif; ?>
+            <?php if ( !empty($this->settings['social_twitter_link'])) : ?>
+            <div class="twitter">
+              <a target="_blank" title="Twitter oldalunk" href="<?=$this->settings['social_twitter_link']?>"><i class="fa fa-twitter"></i></a>
+            </div>
+            <?php endif; ?>
+          </div>
+        </div>
+        <div class="actions" ng-controller="ActionButtons">
+          <div class="flex flexmob-exc-resp">
+            <div class="visszahivas">
+              <button type="button" ng-click="requestRecall()">Ingyenes visszahívás</button>
+            </div>
+            <div class="ajanlatkeres">
+              <button type="button" ng-click="requestAjanlat()">Ingyenes árajánlat</button>
+            </div>
+          </div>
+        </div>
+        <div class="contact hide-on-mobile">
+          <div class="flex">
+            <div class="telefon">
+              <div class="wrapper">
+                <i class="fa fa-phone"></i>
+                <div class="title">
+                  Telefon:
+                </div>
+                <div class="val">
+                  <a href="tel:<?=$this->settings['page_author_phone']?>"><?=$this->settings['page_author_phone']?></a>
+                </div>
+              </div>
+            </div>
+            <div class="email">
+              <div class="wrapper">
+                <i class="fa fa-envelope-o"></i>
+                <div class="title">
+                  E-mail:
+                </div>
+                <div class="val">
+                  <a href="mailto:<?=$this->settings['primary_email']?>"><?=$this->settings['primary_email']?></a>
+                </div>
+              </div>
+            </div>
+            <div class="address">
+              <div class="wrapper">
+                <i class="fa fa-map-marker"></i>
+                <div class="title">
+                  Cím:
+                </div>
+                <div class="val">
+                  <?=$this->settings['page_author_address']?>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="tudastar hide-on-mobile">
+          <div class="flex">
+            <div class="ico">
+              <div class="wrap">
+                  <i class="fa fa-lightbulb-o"></i>
+              </div>
+            </div>
+            <div class="text">
+              Tudástár
+            </div>
+            <div class="dropmenu">
+              <div class="dropmenu-container">
+                <div class="flex flexmob-exc-resp">
+                  <div class="text">
+                    Válasszon témakört
+                  </div>
+                  <div class="arrow">
+                    <i class="fa fa-angle-down"></i>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
   <div class="main">
     <div class="pw">
-      ---
+      <div class="flex">
+        <div class="logo">
+          <a href="<?=$this->settings['page_url']?>"><img src="<?=IMG?>tuzvedelmicentrum_logo.svg" alt="<?=$this->settings['page_title']?>"></a>
+        </div>
+        <div class="searcher">
+          <div class="searchform">
+            <form class="" action="/termekek/" method="get">
+            <div class="flex flexmob-exc-resp">
+              <div class="input">
+                <input type="text" name="src" value="" placeholder="Keresési kifejezés megadása">
+              </div>
+              <div class="button">
+                <button type="submit"><i class="fa fa-search"></i> Keresés</button>
+              </div>
+            </div>
+            </form>
+          </div>
+        </div>
+        <div class="actions">
+          <div class="flex">
+            <div class="favorite">
+              <a href="/kedvencek" class="holder">
+                <div class="ico">
+                  <span class="badge">99</span>
+                  <i class="fa fa-heart"></i>
+                </div>
+                Kedvencek
+              </a>
+            </div>
+            <div class="div">&nbsp;</div>
+            <div class="account">
+              <a href="/user/belepes" class="holder">
+                <div class="ico">
+                  <img src="<?=IMG?>icons/lock.svg" alt="Belépés">
+                </div>
+                Belépés
+              </a>
+            </div>
+            <div class="div"></div>
+            <div class="cart">
+              <a href="/kosar" class="holder">
+                <div class="flex">
+                  <div class="ico">
+                    <span class="badge">99</span>
+                    <img src="<?=IMG?>icons/cart.svg" alt="Kosár">
+                  </div>
+                  <div class="info">
+                    <div class="h">Kosár tartalom</div>
+                    <div class="l">Összeg</div>
+                    <div class="cash"><span class="amount">123 456</span> Ft</div>
+                  </div>
+                </div>
+              </a>
+            </div>
+            <div class="div"></div>
+            <div class="contact">
+              <a href="/kapcsolat" class="holder">
+                <div class="ico">
+                  <i class="fa fa-fire-extinguisher"></i>
+                </div>
+                Kapcsolat
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
   <div class="bottom">
