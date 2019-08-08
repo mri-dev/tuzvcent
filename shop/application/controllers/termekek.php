986cebb0f074ee48906a32e55710c933ef290dbd
mri-dev <pocokxtrame@gmail.com>
2019-08-08 17:03:29 +0200

    Save

--- shop/application/controllers/termekek.php
+++ shop/application/controllers/termekek.php
@@ -175,7 +175,7 @@ class termekek extends Controller {
 			// SEO Információk
 			$SEO = null;
 			// Site info
-			$SEO .= $this->view->addMeta('description', 'Minőségi '.strtolower($this->view->category->getName()) . ' a Casada Hungary Kft.-től. Őrizze meg egészségét!');
+			$SEO .= $this->view->addMeta('description', 'Minőségi '.($this->view->category->getName()) . ' a(z) '.$this->view->settings['page_title'].'-tól!');
 			$SEO .= $this->view->addMeta('keywords',$this->view->category->getName());
 			$SEO .= $this->view->addMeta('revisit-after','3 days');
 
